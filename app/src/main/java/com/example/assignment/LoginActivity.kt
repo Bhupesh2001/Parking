b31b3986ceeb7b1e3d44dfbe65e2238a295b95ba
bpatt <bpattaniak@gmail.com>
2021-01-05 13:35:28 +0530

    Added dropdown in states

--- app/src/main/java/com/example/assignment/LoginActivity.kt
+++ app/src/main/java/com/example/assignment/LoginActivity.kt
@@ -6,65 +6,23 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var txtRegister:TextView
-    lateinit var etPhone:EditText
-    lateinit var etPassword:EditText
-    lateinit var txtLogin:TextView
-    lateinit var txtFogotPassword:TextView
+    lateinit var googleLogin:Button
     lateinit var sharedPreferces:SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        googleLogin = findViewById(R.id.btnLoginByGoogle)
 
-        sharedPreferces = getSharedPreferences("Preferences",Context.MODE_PRIVATE)
-        val isLoggedIn = sharedPreferces.getBoolean("isLoggedIn",false)
-        if (isLoggedIn)
-        {
-            val intent = Intent(this@LoginActivity,HomeActivity::class.java)
+        googleLogin.setOnClickListener{
+            val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
             startActivity(intent)
             super.onPause()
             finish()
         }
-//        etPhone = findViewById(R.id.etPhone)
-//        etPassword = findViewById(R.id.etPassword)
-//        txtRegister = findViewById(R.id.txtRegister)
-//        txtLogin = findViewById(R.id.txtLogin)
-//        txtFogotPassword = findViewById(R.id.txtFogotPassword)
 
-        txtRegister.setOnClickListener {
-            val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
-            startActivity(intent)
-//            super.onPause()
-//            finish()
-        }
-
-        txtLogin.setOnClickListener {
-            val num = sharedPreferces.getString("etMobile","0")
-            val pass = sharedPreferces.getString("etPassword","0")
-            if(num == etPhone.text.toString() && pass == etPassword.text.toString())
-            {
-                sharedPreferces.edit().putBoolean("isLoggedIn",true).apply()
-                val intent = Intent(this@LoginActivity,HomeActivity::class.java)
-                startActivity(intent)
-                super.onPause()
-                finish()
-            }
-            else
-            {
-                Toast.makeText(this@LoginActivity, "Invalid Inputs", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-        txtFogotPassword.setOnClickListener {
-            val intent = Intent(this@LoginActivity,ForgotActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
--- app/src/main/java/com/example/assignment/RegisterActivity.kt
+++ app/src/main/java/com/example/assignment/RegisterActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var etName:EditText
@@ -18,47 +16,54 @@ class RegisterActivity : AppCompatActivity() {
     lateinit var etConfirmPassword:EditText
     lateinit var btnLogin:Button
     lateinit var sharedPreferences:SharedPreferences
+    lateinit var atv:AutoCompleteTextView
+    val states = arrayListOf("Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka",
+                             "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana",
+                             "Tripura", "Uttarakhand", "Uttar Pradesh", "West Bengal", "Andaman and Nicobar Islands", "Chandigarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi",
+                             "Lakshadweep", "Puducherry")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
-        etName = findViewById(R.id.etName)
-        etEmail = findViewById(R.id.etEmail)
-        etMobile = findViewById(R.id.etMobile)
-        etAddress = findViewById(R.id.etAddress)
-        etRegisterPassword = findViewById(R.id.etRegisterPassword)
-        etConfirmPassword = findViewById(R.id.etConfirmPassword)
-        btnLogin = findViewById(R.id.btnLogin)
-
-        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name),Context.MODE_PRIVATE)
-
-        btnLogin.setOnClickListener {
-            if (etName.text.toString().length < 3)
-                Toast.makeText(this@RegisterActivity,"Name is too short", Toast.LENGTH_SHORT).show()
-
-            else if (!(etEmail.text.toString().contains('@')) && !(etEmail.text.toString().contains('.')))
-                Toast.makeText(this@RegisterActivity,"Invalid Email", Toast.LENGTH_SHORT).show()
-
-            else if (etMobile.text.length !=10)
-                Toast.makeText(this@RegisterActivity,"Invalid Phone", Toast.LENGTH_SHORT).show()
-            
-            else if (etRegisterPassword.text.toString()==etConfirmPassword.text.toString() && etConfirmPassword.text.toString().isNotEmpty())
-            {
-                sharedPreferences.edit().putBoolean("isLoggedIn",true).apply()
-                sharedPreferences.edit().putString("etName", etName.text.toString()).apply()
-                sharedPreferences.edit().putString("etEmail", etEmail.text.toString()).apply()
-                sharedPreferences.edit().putString("etMobile", etMobile.text.toString()).apply()
-                sharedPreferences.edit().putString("etAddress", etAddress.text.toString()).apply()
-                sharedPreferences.edit().putString("etPassword", etRegisterPassword.text.toString()).apply()
-                val intent = Intent(this@RegisterActivity,HomeActivity::class.java)
-                startActivity(intent)
-                super.onPause()
-                finish()
-            }
-            else
-                Toast.makeText(this@RegisterActivity,"Password Not match", Toast.LENGTH_SHORT).show()
-        }
-
+        atv = findViewById(R.id.etState)
+        atv.setAdapter(ArrayAdapter<String>(this@RegisterActivity,R.layout.support_simple_spinner_dropdown_item,states))
     }
 }
+
+//
+//        etName = findViewById(R.id.etName)
+//        etEmail = findViewById(R.id.etEmail)
+//        etMobile = findViewById(R.id.etMobile)
+//        etAddress = findViewById(R.id.etAddress)
+//        etRegisterPassword = findViewById(R.id.etRegisterPassword)
+//        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+//        btnLogin = findViewById(R.id.btnLogin)
+//
+//        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name),Context.MODE_PRIVATE)
+//
+//        btnLogin.setOnClickListener {
+//            if (etName.text.toString().length < 3)
+//                Toast.makeText(this@RegisterActivity,"Name is too short", Toast.LENGTH_SHORT).show()
+//
+//            else if (!(etEmail.text.toString().contains('@')) && !(etEmail.text.toString().contains('.')))
+//                Toast.makeText(this@RegisterActivity,"Invalid Email", Toast.LENGTH_SHORT).show()
+//
+//            else if (etMobile.text.length !=10)
+//                Toast.makeText(this@RegisterActivity,"Invalid Phone", Toast.LENGTH_SHORT).show()
+//
+//            else if (etRegisterPassword.text.toString()==etConfirmPassword.text.toString() && etConfirmPassword.text.toString().isNotEmpty())
+//            {
+//                sharedPreferences.edit().putBoolean("isLoggedIn",true).apply()
+//                sharedPreferences.edit().putString("etName", etName.text.toString()).apply()
+//                sharedPreferences.edit().putString("etEmail", etEmail.text.toString()).apply()
+//                sharedPreferences.edit().putString("etMobile", etMobile.text.toString()).apply()
+//                sharedPreferences.edit().putString("etAddress", etAddress.text.toString()).apply()
+//                sharedPreferences.edit().putString("etPassword", etRegisterPassword.text.toString()).apply()
+//                val intent = Intent(this@RegisterActivity,HomeActivity::class.java)
+//                startActivity(intent)
+//                super.onPause()
+//                finish()
+//            }
+//            else
+//                Toast.makeText(this@RegisterActivity,"Password Not match", Toast.LENGTH_SHORT).show()
+//        }
