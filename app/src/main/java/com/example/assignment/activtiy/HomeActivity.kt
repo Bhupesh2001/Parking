633c97cfd2317bed1719dd0b5ac557ad5e03d691
bpatt <bpattaniak@gmail.com>
2021-01-08 14:29:32 +0530

    Drawer fixed and added fragments for each item in menu of drawer

---
+++ app/src/main/java/com/example/assignment/activtiy/HomeActivity.kt
@@ -0,0 +1,110 @@
+package com.example.assignment.activtiy
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.assignment.R
+import com.example.assignment.fragments.DashboardFragment
+import com.example.assignment.fragments.FavoriteFragment
+import com.example.assignment.fragments.ProfileFragment
+import com.google.android.material.navigation.NavigationView
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var DrawerLayout: DrawerLayout
+    lateinit var CoordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var Frame: FrameLayout
+    lateinit var navigationView: NavigationView
+
+    var previousMenuItem:MenuItem? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        DrawerLayout = findViewById(R.id.drawerLayout)
+        toolbar = findViewById(R.id.toolbar)
+        Frame = findViewById(R.id.frame)
+        CoordinatorLayout = findViewById(R.id.coordinatorLayout)
+        navigationView = findViewById(R.id.navigationView)
+        setUpToolbar()
+        openDashboard()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this@HomeActivity,
+                DrawerLayout, R.string.open_drawer, R.string.close_drawer)
+
+        DrawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+            if (previousMenuItem !=null)
+                previousMenuItem?.isChecked = false
+            it.isCheckable = true
+            it.isChecked = true
+            previousMenuItem = it
+            when(it.itemId){
+                R.id.home ->{
+                    openDashboard()
+                    DrawerLayout.closeDrawers()
+                }
+                R.id.favorite ->{
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.frame, FavoriteFragment()).commit()
+                    supportActionBar?.title = "Favorite"
+                    DrawerLayout.closeDrawers()
+                }
+                R.id.profile ->{
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.frame, ProfileFragment()).commit()
+                    supportActionBar?.title = "Profile"
+                    DrawerLayout.closeDrawers()
+                }
+                R.id.logout ->{
+//                    todo
+                }
+
+            }
+            return@setNavigationItemSelectedListener true
+        }
+    }
+    fun openDashboard(){
+        val fragment = DashboardFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame,fragment).commit()
+        supportActionBar?.title = "Dashboard"
+        navigationView.setCheckedItem(R.id.home)
+    }
+    fun setUpToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="Toolbar Title"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == android.R.id.home){
+            DrawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        val frag = supportFragmentManager.findFragmentById(R.id.frame)
+        when(frag){
+            !is DashboardFragment -> openDashboard()
+            else -> super.onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/assignment/activtiy/RegisterActivity.kt
+++ app/src/main/java/com/example/assignment/activtiy/RegisterActivity.kt
@@ -1,12 +1,9 @@
-package com.example.assignment
+package com.example.assignment.activtiy
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import com.google.firebase.auth.FirebaseAuth
+import com.example.assignment.R
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,7 +17,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         atv = findViewById(R.id.etState)
-        atv.setAdapter(ArrayAdapter<String>(this@RegisterActivity,R.layout.support_simple_spinner_dropdown_item,states))
+        atv.setAdapter(ArrayAdapter<String>(this@RegisterActivity, R.layout.support_simple_spinner_dropdown_item,states))
     }
 }
 
